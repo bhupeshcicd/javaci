8b149f12ea5e1bf60db0df9dbeabef466c7eaf28
bhupeshcicd <bhupeshdeora129@gmail.com>
2025-06-09 16:15:22 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Even death I'm the Hero");
+        System.out.println("Even death I'm the Hero SCM CI");
     }
 }
